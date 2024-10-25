bf3ed94d96eab2644d030f4860eb596d15abab9f
simycodes <simycodes@gmail.com>
2024-10-25 21:08:13 +0200

    week-6 assignment-code-completion

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,11 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        // CHECK IF VALUE IS A DUPLICATE, DON'T ADD IT.
+        if(value == Data) 
+        {
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +39,78 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        // CHECK ROOT VALUE
+        if(value == Data)
+        {
+            return true;
+        }
+
+        // CHECK THE LEFT SUB TREE AND THEN THE RIGHT SUB TREE
+        else if(value < Data) 
+        {
+            
+            if (Left != null)
+            {
+                return Left.Contains(value);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        else 
+        {
+
+            if (Right != null)
+            {
+                return Right.Contains(value);
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight;
+        int rightHeight;
+
+        // HEIGHT OF LEFT SUBTREE
+        if (Left != null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+        else
+        {
+            leftHeight = 0;
+        }
+
+        // HEIGHT OF RIGHT SUBTREE
+        if (Right != null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+        else
+        {
+            rightHeight = 0;
+        }
+
+        // HEIGHT OF TREE
+        if (leftHeight > rightHeight)
+        {
+            return 1 + leftHeight;
+        }
+        else
+        {
+            return 1 + rightHeight;
+        }
+        // return 0; // Replace this line with the correct return statement(s)
     }
+
 }
